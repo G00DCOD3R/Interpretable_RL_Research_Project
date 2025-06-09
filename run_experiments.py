4ff498a9341156a561ed1ded887e2bfe39b4a0b4
Tymon <t.s.cichocki@student.tudelft.nl>
2025-06-09 11:55:43 +0200

    working swimmer

---
+++ run_experiments.py
@@ -0,0 +1,12 @@
+import subprocess
+from multiprocessing import Pool
+
+def run_dagger(n):
+    print(f"Starting dagger.py with n={n}")
+    subprocess.run(["python3", "dagger.py", str(n)])
+    print(f"Finished dagger.py with n={n}")
+
+if __name__ == "__main__":
+    ns = list(range(4, 30))  
+    with Pool(processes=len(ns)) as pool:
+        pool.map(run_dagger, ns)
